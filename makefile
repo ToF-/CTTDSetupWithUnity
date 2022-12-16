ccfc269b2c8cd16ae31d8eb1b90758d44268a2e9
Christophe Thibaut <cthibaut@octo.com>
2022-12-16 07:45:35 +0100

    added memory testing

--- makefile
+++ makefile
@@ -9,7 +9,7 @@ UNITY_ROOT=unity
 
 TARGET=test.out
 
-SRC_FILES=$(UNITY_ROOT)/src/unity.c $(UNITY_ROOT)/extras/fixture/src/unity_fixture.c  test/my_tests.c test/test_runner.c test/all_tests.c src/my_code.c
+SRC_FILES=$(UNITY_ROOT)/src/unity.c $(UNITY_ROOT)/extras/memory/src/unity_memory.c $(UNITY_ROOT)/extras/fixture/src/unity_fixture.c  test/my_tests.c test/test_runner.c test/all_tests.c src/my_code.c
 
 INC_DIRS=-Isrc -I$(UNITY_ROOT)/src -I$(UNITY_ROOT)/extras/fixture/src -I$(UNITY_ROOT)/extras/memory/src
 
--- test/my_tests.c
+++ test/my_tests.c
@@ -1,10 +1,23 @@
 #include "unity_fixture.h"
+#include "unity.h"
+#include "unity_memory.h"
+#include "stdlib.h"
 #include "my_code.h"
 TEST_GROUP(my_code);
 
-TEST_SETUP(my_code) {}
-TEST_TEAR_DOWN(my_code) {}
+int *an_int;
+
+TEST_SETUP(my_code) {
+    UnityMalloc_StartTest();
+    an_int = (int *)malloc(sizeof(int));
+}
+
+TEST_TEAR_DOWN(my_code) {
+    free(an_int);
+    UnityMalloc_EndTest();
+}
 
 TEST(my_code,dummy_test){
-    TEST_ASSERT_EQUAL(4, some_function(2));
+    *an_int = 2;
+    TEST_ASSERT_EQUAL(4, some_function(*an_int));
 }
